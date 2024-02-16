e1ff561b20c1ef3199d0f24cbef271b18135ca75
aixion <aixion1506@gmail.com>
2024-02-16 18:08:01 +0900

    파이썬 반복문

---
+++ loops.py
@@ -0,0 +1,25 @@
+# # 반복문 (loops)
+
+
+# # For문
+# i = 0
+# sum = 0
+
+# # for i in range(1,101):
+# #     print(i)
+
+# for i in range (1, 101):
+#     sum = sum + i
+# print(sum)
+
+
+# While문
+
+# while True:
+#         print("while loop")
+
+progress = 0
+
+while progress < 100:
+    progress = progress + 1
+    print(f"{progress}% completed")
