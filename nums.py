ea046d9c0e2e07effa9654ff45b0098c9f85f388
aixion <aixion1506@gmail.com>
2024-02-16 16:23:20 +0900

    파이썬 숫자형 데이터

---
+++ nums.py
@@ -0,0 +1,68 @@
+# a = 2
+# b = 3
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+# # a의 b승
+# print(a ** b)
+
+# # 괄호 먼저 계산
+# print((a + b) *b)
+
+# # 몫을 구함
+# print(a // b)
+
+# # 나머지를 구함
+# print(a % b)
+
+
+
+# # 실수(float) - 십진 부동 소수점
+
+# x = 0.6
+# y = 0.3
+# z = 1
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+# print(x + z)
+# print(x - z)
+# print(x * z)
+# print(x / z)
+
+# # 수치가 큰 데이터인 경우 일의 자리부터 3자리씩 끊어서
+# # 언더바 사용해도 그대로 인식
+# price = 1_234_567_890
+# print(price)
+
+
+# # 상수
+# # 대문자로 변수 이름 지정시 해당 값은 변경하면 안됨
+# PI = 3.14592
+
+# print(PI)
+
+
+# 문자열-숫자 간 변환
+
+a = 100
+b = "100"
+c = "0.123"
+
+# 문자로 취급시
+a = str(a)
+# 숫자로 취급시
+b = int(b)
+# 실수로 취급
+c = float(c)
+
+
+print(a)
+print(b)
+print(c)
